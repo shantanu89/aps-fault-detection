7b1826d11e3fb4a843cec61cbad8837d1cae4cdf
shantanu89 <shantanupradhan89@gmail.com>
2023-07-15 12:40:24 +0530

    data transformation and model trainer component completed

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__=="__main__":
           data_transformation=DataTransformation(data_transformation_config=data_transformation_config, data_ingestion_artifact=data_ingestion_artifact)
           data_transformation_artifact=data_transformation.initiate_data_transformation()
 
-          #model trainer component
+          # model trainer component 
           model_trainer_config=config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
           model_trainer=ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
           model_trainer_artifact=model_trainer.initiate_model_trainer()
