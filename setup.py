44f5229180c17fac99d88070bb62d60984f7bc13
shantanu89 <shantanupradhan89@gmail.com>
2023-07-16 15:42:31 +0530

    updated docker compose and docker ignore file added airflow folder

--- setup.py
+++ setup.py
@@ -14,14 +14,11 @@ def get_requirements()->List[str]:
     
     return requirement_list
 
-
 setup(
-    
     name="sensor",
     version="0.0.1",
     author="shantanu",
     author_email="shantanupradhan89@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements(),
-
 )
